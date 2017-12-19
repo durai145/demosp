218620073e13c6ade885391da82468ce730ca08b
Ubuntu <ubuntu@heaerieglobalsolutions.com>
2017-12-19 03:46:00 +0000

    first commit

---
+++ git.sh
@@ -0,0 +1,6 @@
+echo "# Demo Service Portal" > README.md
+git init
+git add --all
+git commit -m "first commit"
+git remote add origin https://github.com/durai145/demosp.git 
+git push -u origin master
